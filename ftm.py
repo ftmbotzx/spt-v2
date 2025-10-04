eb26b177e92d3cde3d51a6bf5fa6ab977294d682
ftmbotzx <kumarasta91@gmail.com>
2025-10-04 19:17:48 +0530

    Update ftm.py

--- ftm.py
+++ ftm.py
@@ -162,6 +162,9 @@ def download_file_endpoint():
             os.remove(filepath)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Get port from environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    # Run the app, listening on all interfaces
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
